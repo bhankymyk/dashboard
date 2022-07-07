d099dd21038221df6ddd5750a1e1374999b1e8e0
bhankymyk <bankole123michael@gmail.com>
2022-07-07 15:26:58 +0100

    Homepage in progress

--- bloom-dashboard/src/App.js
+++ bloom-dashboard/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row} from 'react-bootstrap'
 import Home from './Component/Home';
 
 
 function App() {
   return (
     <div className="App">
+      <Container>
+      
       <Home/>
+        
+      </Container>
     </div>
   );
 }
--- bloom-dashboard/src/Component/Home.jsx
+++ bloom-dashboard/src/Component/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUserCog, faUserTie, faUserGraduate } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
   return (
-    <div>Homepage</div>
+    <div className='row d-flex justify-items-around'>
+        <div className="col-md-4 col-sm-12 bg-light box-head">
+            <FontAwesomeIcon icon={faUserCog}/>
+         Admin
+        </div>
+        <div className="col-md-4 col-sm-12 bg-light box-head">
+        <FontAwesomeIcon icon={faUserTie}/>
+         Teacher
+        </div>
+        <div className="col-md-4 col-sm-12 bg-light box-head">
+        <FontAwesomeIcon icon={faUserGraduate}/>
+         Student
+        </div>
+         
+
+    </div>
   )
 }
 
